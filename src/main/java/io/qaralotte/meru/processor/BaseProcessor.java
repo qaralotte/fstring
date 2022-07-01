4893e095c01aa2ed5874c1c05a103d6e92ab6bcf
qaralotte <qaralotte@gmail.com>
2022-07-01 16:23:28 +0800

    主分支的第一次提交

---
+++ src/main/java/io/qaralotte/meru/processor/BaseProcessor.java
@@ -0,0 +1,70 @@
+package io.qaralotte.meru.processor;
+
+import com.sun.tools.javac.api.JavacTrees;
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.TreeMaker;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.Names;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import java.lang.reflect.Method;
+
+public abstract class BaseProcessor extends AbstractProcessor {
+
+    /**
+     * 日志输出 (实际上用 sout 也可以)
+     */
+    protected Messager messager;
+
+    /**
+     * 语法树
+     */
+    protected JavacTrees trees;
+
+    /**
+     * 语法树生成器
+     */
+    protected TreeMaker treeMaker;
+
+    /**
+     * 变量名工具
+     */
+    protected Names names;
+
+    /**
+     * 获取 IDEA 环境下的 ProcessingEnvironment
+     */
+    private static ProcessingEnvironment jbUnwrap(ProcessingEnvironment wrapper) {
+        ProcessingEnvironment unwrapped = null;
+        try {
+            final Class<?> apiWrappers = wrapper.getClass().getClassLoader().loadClass("org.jetbrains.jps.javac.APIWrappers");
+            final Method unwrapMethod = apiWrappers.getDeclaredMethod("unwrap", Class.class, Object.class);
+            unwrapped = (ProcessingEnvironment) unwrapMethod.invoke(null, ProcessingEnvironment.class, wrapper);
+        }
+        catch (Throwable ignored) {}
+        return unwrapped != null? unwrapped : wrapper;
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        // super.init(processingEnv);
+        processingEnv = jbUnwrap(processingEnv);
+        this.messager = processingEnv.getMessager();
+        this.trees = JavacTrees.instance(processingEnv);
+        Context context = ((JavacProcessingEnvironment) processingEnv).getContext();
+        this.treeMaker = TreeMaker.instance(context);
+        this.names = Names.instance(context);
+    }
+
+    public JCTree.JCExpression memberAccess(String components) {
+        String[] componentArray = components.split("\\.");
+        JCTree.JCExpression expr = treeMaker.Ident(names.fromString(componentArray[0]));
+        for (int i = 1; i < componentArray.length; i++) {
+            expr = treeMaker.Select(expr, names.fromString(componentArray[i]));
+        }
+        return expr;
+    }
+}
